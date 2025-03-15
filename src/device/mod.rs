81d3e6cd3f319c0e09a402b1ddee962916a30b29
ranishot <ranishot@hotmail.com>
2025-03-15 14:09:35 -0300

    Adding windows capability and changind cargo info

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -2,7 +2,5 @@
 pub mod linux;
 #[cfg(target_os = "linux")]
 pub use crate::device::linux::{command_line, config, debounce};
-#[cfg(target_os = "windows")]
 pub mod windows;
-#[cfg(target_os = "windows")]
 pub use crate::device::windows::{command_line, config, debounce};
