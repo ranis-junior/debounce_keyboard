fee4e7a90319669d8cdd273c1b58e8ccf626ee2d
ranishot <ranishot@hotmail.com>
2025-04-25 14:53:35 -0300

    Just test

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,28 @@
 use std::ffi::c_void;
 use std::mem::zeroed;
-use windows::core::{s, Error, PCSTR};
 use windows::Win32::Foundation::{HINSTANCE, HWND, LPARAM, LRESULT, WPARAM};
-use windows::Win32::UI::Input::{GetRawInputData, RegisterRawInputDevices, HRAWINPUT, RAWINPUT, RAWINPUTDEVICE, RAWINPUTHEADER, RIDEV_INPUTSINK, RIDEV_NOLEGACY, RID_INPUT, RIM_TYPEKEYBOARD};
-use windows::Win32::UI::WindowsAndMessaging::{
-    CreateWindowExA, DefWindowProcA, DispatchMessageA, GetMessageA, RegisterClassExA, TranslateMessage,
-    CW_USEDEFAULT, HWND_MESSAGE, MSG, WM_INPUT, WNDCLASSEXA, WS_OVERLAPPEDWINDOW,
+use windows::Win32::UI::Input::{
+    GetRawInputData, HRAWINPUT, RAWINPUT, RAWINPUTDEVICE, RAWINPUTHEADER, RID_INPUT,
+    RIDEV_INPUTSINK, RIDEV_NOLEGACY, RIM_TYPEKEYBOARD, RegisterRawInputDevices,
 };
+use windows::Win32::UI::WindowsAndMessaging::{
+    CW_USEDEFAULT, CreateWindowExA, DefWindowProcA, DispatchMessageA, GetMessageA, HWND_MESSAGE,
+    MSG, RegisterClassExA, TranslateMessage, WM_INPUT, WNDCLASSEXA, WS_OVERLAPPEDWINDOW,
+};
+use windows::core::{Error, PCSTR, s};
 
 use regex::Regex;
 use windows::Win32::Devices::DeviceAndDriverInstallation::{
-    SetupDiDestroyDeviceInfoList, SetupDiEnumDeviceInfo, SetupDiGetClassDevsW, SetupDiGetDeviceInstanceIdW, SetupDiGetDevicePropertyW,
-    SetupDiGetDeviceRegistryPropertyW, DIGCF_DEVICEINTERFACE, DIGCF_PRESENT,
-    HDEVINFO, SPDRP_DEVICEDESC, SP_DEVINFO_DATA,
+    DIGCF_DEVICEINTERFACE, DIGCF_PRESENT, HDEVINFO, SP_DEVINFO_DATA, SPDRP_DEVICEDESC,
+    SetupDiDestroyDeviceInfoList, SetupDiEnumDeviceInfo, SetupDiGetClassDevsW,
+    SetupDiGetDeviceInstanceIdW, SetupDiGetDevicePropertyW, SetupDiGetDeviceRegistryPropertyW,
 };
 use windows::Win32::Devices::HumanInterfaceDevice::GUID_DEVINTERFACE_KEYBOARD;
 use windows::Win32::Devices::Properties::{
-    DEVPKEY_Device_Driver, DEVPKEY_Device_FriendlyName, DEVPROPTYPE, DEVPROP_TYPE_GUID,
-    DEVPROP_TYPE_STRING,
+    DEVPKEY_Device_Driver, DEVPKEY_Device_FriendlyName, DEVPROP_TYPE_GUID, DEVPROP_TYPE_STRING,
+    DEVPROPTYPE,
 };
-use windows::Win32::Foundation::{GetLastError, ERROR_NO_MORE_ITEMS, HANDLE, MAX_PATH};
+use windows::Win32::Foundation::{ERROR_NO_MORE_ITEMS, GetLastError, HANDLE, MAX_PATH};
 use windows::Win32::UI::Input::{
     GetRawInputDeviceInfoW, GetRawInputDeviceList, RAWINPUTDEVICELIST, RIDI_DEVICENAME,
 };
@@ -339,7 +342,7 @@ fn format_device_path(id: &str) -> String {
 //     }
 // }
 
-unsafe extern "system" fn wind_proc(
+unsafe extern "system" fn win_proc(
     h_wnd: HWND,
     msg: u32,
     w_param: WPARAM,
@@ -375,9 +378,13 @@ unsafe extern "system" fn wind_proc(
 
         if raw_input.header.dwType == RIM_TYPEKEYBOARD.0 {
             let keyboard = raw_input.data.keyboard;
+            if keyboard.Flags == 1 {
+                // if it's a key release, we just throw it out the window :D
+                return LRESULT(0);
+            }
             println!(
-                "Tecla: {:?} | hDevice: {:?}",
-                keyboard.VKey, raw_input.header.hDevice
+                "Tecla: {:?} | hDevice: {:?} | flag: {:?}",
+                keyboard.VKey, raw_input.header.hDevice, keyboard.Flags
             );
         }
     }
@@ -388,7 +395,7 @@ unsafe extern "system" fn wind_proc(
 fn define_window_class(lpsz_class_name: PCSTR, h_instance: HINSTANCE) {
     let wcx: WNDCLASSEXA = WNDCLASSEXA {
         cbSize: size_of::<WNDCLASSEXA>() as u32,
-        lpfnWndProc: Some(wind_proc),
+        lpfnWndProc: Some(win_proc),
         hInstance: h_instance,
         lpszClassName: lpsz_class_name,
         ..unsafe { zeroed() }
